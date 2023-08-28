ee37e84c8012ef8f93838d049ec748976f3db37f
Manish Narayan <manish.amhs@gmail.com>
2023-08-28 15:37:04 +0530

    frontend basics done

---
+++ crm_ubercrypto/src/views/dashboard/Dashboard.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <div class="columns">
+            <div class="column is-4 is-narror-4">
+                <h1 class="title">Dashboard</h1>
+
+                
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Dashboard'
+}
+
+</script>
